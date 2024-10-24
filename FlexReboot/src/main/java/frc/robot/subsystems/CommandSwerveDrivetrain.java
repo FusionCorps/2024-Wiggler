88b9f6eb474a4c8c4216d5e59002f3b9872e0831
rishabhreng <rishabhreng@gmail.com>
2024-10-24 17:44:19 -0500

    refactoring

--- FlexReboot/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ FlexReboot/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -1,13 +1,17 @@
 package frc.robot.subsystems;
 
-import java.util.function.Supplier;
+import static frc.robot.Constants.SwerveConstants.*;
 
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.SteerRequestType;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
-
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.FieldCentric;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.PointWheelsAt;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.SwerveDriveBrake;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -15,69 +19,111 @@ import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
 
 /**
- * Class that extends the Phoenix SwerveDrivetrain class and implements
- * subsystem so it can be used in command-based projects easily.
+ * Class that extends the Phoenix SwerveDrivetrain class and implements subsystem so it can be used
+ * in command-based projects easily.
  */
 public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsystem {
-    private static final double kSimLoopPeriod = 0.005; // 5 ms
-    private Notifier m_simNotifier = null;
-    private double m_lastSimTime;
+  private static final double kSimLoopPeriod = 0.005; // 5 ms
+  private Notifier m_simNotifier = null;
+  private double m_lastSimTime;
 
-    /* Blue alliance sees forward as 0 degrees (toward red alliance wall) */
-    // private final Rotation2d BlueAlliancePerspectiveRotation = Rotation2d.fromDegrees(0);
-    // /* Red alliance sees forward as 180 degrees (toward blue alliance wall) */
-    // private final Rotation2d RedAlliancePerspectiveRotation = Rotation2d.fromDegrees(180);
-    // /* Keep track if we've ever applied the operator perspective before or not */
-    // private boolean hasAppliedOperatorPerspective = false;
+  FieldCentric fieldCentricRequest =
+      new FieldCentric()
+          .withSteerRequestType(SteerRequestType.MotionMagic)
+          .withDriveRequestType(DriveRequestType.OpenLoopVoltage)
+          .withDeadband(MaxSpeed * 0.1)
+          .withRotationalDeadband(MaxAngularRate * 0.1);
 
-    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency, SwerveModuleConstants... modules) {
-        super(driveTrainConstants, OdometryUpdateFrequency, modules);
-        if (Utils.isSimulation()) {
-            startSimThread();
-        }
+  SwerveDriveBrake brakeRequest = new SwerveDriveBrake();
+  PointWheelsAt pointWheelsAtRequest = new PointWheelsAt();
+
+  /* Blue alliance sees forward as 0 degrees (toward red alliance wall) */
+  private final Rotation2d BlueAlliancePerspectiveRotation = Rotation2d.fromDegrees(0);
+  /* Red alliance sees forward as 180 degrees (toward blue alliance wall) */
+  private final Rotation2d RedAlliancePerspectiveRotation = Rotation2d.fromDegrees(180);
+  /* Keep track if we've ever applied the operator perspective before or not */
+  private boolean hasAppliedOperatorPerspective = false;
+
+  public CommandSwerveDrivetrain(
+      SwerveDrivetrainConstants driveTrainConstants,
+      double OdometryUpdateFrequency,
+      SwerveModuleConstants... modules) {
+    super(driveTrainConstants, OdometryUpdateFrequency, modules);
+    if (Utils.isSimulation()) {
+      startSimThread();
     }
-    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
-        super(driveTrainConstants, modules);
-        if (Utils.isSimulation()) {
-            startSimThread();
-        }
+  }
+
+  public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
+    return run(() -> this.setControl(requestSupplier.get()));
+  }
+
+  private void startSimThread() {
+    m_lastSimTime = Utils.getCurrentTimeSeconds();
+
+    /* Run simulation at a faster rate so PID gains behave more reasonably */
+    m_simNotifier =
+        new Notifier(
+            () -> {
+              final double currentTime = Utils.getCurrentTimeSeconds();
+              double deltaTime = currentTime - m_lastSimTime;
+              m_lastSimTime = currentTime;
+
+              /* use the measured time delta, get battery voltage from WPILib */
+              updateSimState(deltaTime, RobotController.getBatteryVoltage());
+            });
+    m_simNotifier.startPeriodic(kSimLoopPeriod);
+  }
+
+  @Override
+  public void periodic() {
+    /* Periodically try to apply the operator perspective
+     * If we haven't applied the operator perspective before, then we should apply it regardless of DS state
+     * This allows us to correct the perspective in case the robot code restarts mid-match
+     * Otherwise, only check and apply the operator perspective if the DS is disabled
+     * This ensures driving behavior doesn't change until an explicit disable event occurs during testing */
+    if (!hasAppliedOperatorPerspective || DriverStation.isDisabled()) {
+      DriverStation.getAlliance()
+          .ifPresent(
+              (allianceColor) -> {
+                this.setOperatorPerspectiveForward(
+                    allianceColor == Alliance.Red
+                        ? RedAlliancePerspectiveRotation
+                        : BlueAlliancePerspectiveRotation);
+                hasAppliedOperatorPerspective = true;
+              });
     }
+  }
 
-    public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
-        return run(() -> this.setControl(requestSupplier.get()));
-    }
+  /**
+   * Run the swerve drivetrain in field-centric mode.
+   *
+   * @param forward
+   * @param strafe
+   * @param rotate
+   * @return Command
+   */
+  public Command runSwerveFC(DoubleSupplier forward, DoubleSupplier strafe, DoubleSupplier rotate) {
+    return applyRequest(
+        () ->
+            fieldCentricRequest
+                .withVelocityX(forward.getAsDouble())
+                .withVelocityY(strafe.getAsDouble())
+                .withRotationalRate(rotate.getAsDouble()));
+  }
 
-    private void startSimThread() {
-        m_lastSimTime = Utils.getCurrentTimeSeconds();
+  public Command brake() {
+    return applyRequest(() -> brakeRequest);
+  }
 
-        /* Run simulation at a faster rate so PID gains behave more reasonably */
-        m_simNotifier = new Notifier(() -> {
-            final double currentTime = Utils.getCurrentTimeSeconds();
-            double deltaTime = currentTime - m_lastSimTime;
-            m_lastSimTime = currentTime;
-
-            /* use the measured time delta, get battery voltage from WPILib */
-            updateSimState(deltaTime, RobotController.getBatteryVoltage());
-        });
-        m_simNotifier.startPeriodic(kSimLoopPeriod);
-    }
-
-    @Override
-    public void periodic() {
-        /* Periodically try to apply the operator perspective */
-        /* If we haven't applied the operator perspective before, then we should apply it regardless of DS state */
-        /* This allows us to correct the perspective in case the robot code restarts mid-match */
-        /* Otherwise, only check and apply the operator perspective if the DS is disabled */
-        /* This ensures driving behavior doesn't change until an explicit disable event occurs during testing*/
-        // if (!hasAppliedOperatorPerspective || DriverStation.isDisabled()) {
-        //     DriverStation.getAlliance().ifPresent((allianceColor) -> {
-        //         this.setOperatorPerspectiveForward(
-        //                 allianceColor == Alliance.Red ? RedAlliancePerspectiveRotation
-        //                         : BlueAlliancePerspectiveRotation);
-        //         hasAppliedOperatorPerspective = true;
-        //     });
-        // }
-    }
+  public Command pointWheelsAt(DoubleSupplier x, DoubleSupplier y) {
+    return applyRequest(
+        () ->
+            pointWheelsAtRequest.withModuleDirection(
+                new Rotation2d(x.getAsDouble(), y.getAsDouble())));
+  }
 }
