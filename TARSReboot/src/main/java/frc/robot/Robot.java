986143381d5ca32e11880529be77ca54bca661a4
rishabhreng <rishabhreng@gmail.com>
2024-11-12 19:52:21 -0600

    TARSReboot-added working open-loop swerve

--- TARSReboot/src/main/java/frc/robot/Robot.java
+++ TARSReboot/src/main/java/frc/robot/Robot.java
@@ -4,30 +4,73 @@
 
 package frc.robot;
 
-import static frc.robot.Constants.OperatorConstants.kDriverControllerPort;
-
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.subsystems.Drivetrain;
 
 public class Robot extends TimedRobot {
-  private final CommandXboxController controller;
-  private final Drivetrain drtvetrain;
+  private Command m_autonomousCommand;
 
-  public Robot() {
-    controller = new CommandXboxController(kDriverControllerPort);
-    drtvetrain = new Drivetrain();
+  private RobotContainer m_robotContainer;
 
-    drtvetrain.setDefaultCommand(
-        drtvetrain.runSwerveFC(
-            () -> -controller.getLeftY(),
-            () -> controller.getLeftX(),
-            () -> controller.getRightX()));
+  @Override
+  public void robotInit() {
+    m_robotContainer = new RobotContainer();
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
   }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void disabledExit() {}
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void autonomousExit() {}
+
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void teleopExit() {}
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void testExit() {}
+
+  @Override
+  public void simulationPeriodic() {}
 }
