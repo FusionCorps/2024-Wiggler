e8f21384375d859ca29aa02a50d83c692ac215ec
rishabhreng <rishabhreng@gmail.com>
2024-11-12 20:09:34 -0600

    FlexReboot-modify intake code/add button-binding

--- FlexReboot/src/main/java/frc/robot/RobotContainer.java
+++ FlexReboot/src/main/java/frc/robot/RobotContainer.java
@@ -16,13 +16,13 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.Intake;
 
 public class RobotContainer {
   /* Setting up bindings for necessary control of the swerve drive platform */
   private final CommandXboxController joystick = new CommandXboxController(0); // My joystick
   private final CommandSwerveDrivetrain drivetrain = DriveTrain; // My drivetrain
-
-  // TODO: add Intake and bind its running command to a button
+  private final Intake intake = new Intake();
 
   private final Telemetry logger = new Telemetry(MaxSpeed);
 
@@ -32,13 +32,10 @@ public class RobotContainer {
             () -> -joystick.getLeftY() * MaxSpeed,
             () -> -joystick.getLeftX() * MaxSpeed,
             () -> -joystick.getRightX() * MaxAngularRate));
-    joystick.a().whileTrue(drivetrain.brake());
-    joystick
-        .b()
-        .whileTrue(
-            drivetrain.pointWheelsAt(() -> -joystick.getLeftY(), () -> -joystick.getLeftX()));
+    
     // reset the field-centric heading on left bumper press
-    joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
+    joystick.b().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
+    joystick.a().whileTrue(intake.runIntake());
 
     if (Utils.isSimulation()) {
       drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
--- FlexReboot/src/main/java/frc/robot/subsystems/Intake.java
+++ FlexReboot/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,8 +1,9 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkBase.IdleMode;
+
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -13,15 +14,12 @@ public class Intake extends SubsystemBase {
   public Intake() {
     /* Configure smart current limit  */
     intakeMotor.setSmartCurrentLimit(80);
+    intakeMotor.setIdleMode(IdleMode.kCoast);
     intakeMotor.burnFlash();
   }
 
-  // simply set the speed of the intake motor to 80% output (lazy output mode, no need to use
-  // setpoint)
+  // set the speed of the intake motor to 80% output when running
   public Command runIntake() {
-    return Commands.run(
-        () -> {
-          intakeMotor.set(0.8);
-        });
+    return runEnd(() -> intakeMotor.set(0.8), () -> intakeMotor.set(0));
   }
 }
