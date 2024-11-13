ca87d57ab7374e33933c3e100b967165d78117c7
Samuel <sasdallas765@gmail.com>
2024-11-12 22:29:29 -0600

    actually do stuff
    i am ill

--- FlexReboot/src/main/java/frc/robot/subsystems/Intake.java
+++ FlexReboot/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,21 +1,27 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkFlex;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
-  // TODO: add intake motor - use REV CANSparkFlex, id 6
-  CANSparkFlex intakeMotor = new CANSparkFlex(6, CANSparkFlex.MotorType.kBrushless);
-  
-  // TODO: configure intake motor
-  public Intake() {}
+  CANSparkFlex intakeMotor =
+      new CANSparkFlex(Constants.kCANMotorID, CANSparkFlex.MotorType.kBrushless);
 
-  // TODO: implement intake Command
-  // simply set the speed of the intake motor to 80% output (lazy output mode, no need to use setpoint)
+  public Intake() {
+    /* Configure smart current limit  */
+    intakeMotor.setSmartCurrentLimit(80);
+    intakeMotor.burnFlash();
+  }
+
+  // simply set the speed of the intake motor to 80% output (lazy output mode, no need to use
+  // setpoint)
   public Command runIntake() {
-    return Commands.none();
+    return Commands.run(
+        () -> {
+          intakeMotor.set(0.8);
+        });
   }
 }
