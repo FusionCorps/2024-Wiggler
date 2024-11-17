dbf0c5055130228cd9b0088edf5a6f8320ee73c3
rishabhreng <rishabhreng@gmail.com>
2024-11-17 12:17:51 -0600

    add spotless to immigrated projects

--- PrimusReboot/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ PrimusReboot/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,12 +4,11 @@
  */
 package frc.robot.subsystems;
 
-import java.util.function.DoubleSupplier;
+import static frc.robot.Constants.MotorConstants.*;
 
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -17,77 +16,80 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants;
-import static frc.robot.Constants.MotorConstants.*;
+import java.util.function.DoubleSupplier;
 
 public class DriveTrain extends SubsystemBase {
 
-    /**
-     * Motor variables
-     */
-    private final CANSparkMax m_frontRightMotor = new CANSparkMax(FRONT_RIGHT_MOTOR_PORT, MotorType.kBrushless);
-    private final CANSparkMax m_frontLeftMotor = new CANSparkMax(BACK_LEFT_MOTOR_PORT, MotorType.kBrushless);
-    private final CANSparkMax m_backLeftMotor = new CANSparkMax(BACK_LEFT_MOTOR_PORT, MotorType.kBrushless);
-    private final CANSparkMax m_backRightMotor = new CANSparkMax(FRONT_RIGHT_MOTOR_PORT, MotorType.kBrushless);
+  /** Motor variables */
+  private final CANSparkMax m_frontRightMotor =
+      new CANSparkMax(FRONT_RIGHT_MOTOR_PORT, MotorType.kBrushless);
 
-    /**
-     * Drive variables
-     */
-    private final DifferentialDrive m_drive = new DifferentialDrive(m_frontLeftMotor, m_frontRightMotor); // Main robot drive
+  private final CANSparkMax m_frontLeftMotor =
+      new CANSparkMax(BACK_LEFT_MOTOR_PORT, MotorType.kBrushless);
+  private final CANSparkMax m_backLeftMotor =
+      new CANSparkMax(BACK_LEFT_MOTOR_PORT, MotorType.kBrushless);
+  private final CANSparkMax m_backRightMotor =
+      new CANSparkMax(FRONT_RIGHT_MOTOR_PORT, MotorType.kBrushless);
 
-    // Trigger for full speed
-    public final Trigger atFullSpeed = new Trigger(() -> MathUtil.isNear(0.5, m_frontLeftMotor.get(), 0.05));
+  /** Drive variables */
+  private final DifferentialDrive m_drive =
+      new DifferentialDrive(m_frontLeftMotor, m_frontRightMotor); // Main robot drive
 
-    /**
-     * Creates a new DriveTrain.
-     */
-    public DriveTrain() {
-        m_frontLeftMotor.restoreFactoryDefaults();
-        m_frontRightMotor.restoreFactoryDefaults();
-        m_backLeftMotor.restoreFactoryDefaults();
-        m_backRightMotor.restoreFactoryDefaults();
+  // Trigger for full speed
+  public final Trigger atFullSpeed =
+      new Trigger(() -> MathUtil.isNear(0.5, m_frontLeftMotor.get(), 0.05));
 
-        // Setup inversion
-        m_frontRightMotor.setInverted(true);
+  /** Creates a new DriveTrain. */
+  public DriveTrain() {
+    m_frontLeftMotor.restoreFactoryDefaults();
+    m_frontRightMotor.restoreFactoryDefaults();
+    m_backLeftMotor.restoreFactoryDefaults();
+    m_backRightMotor.restoreFactoryDefaults();
 
-        // Setup primary and follower controllers
-        m_backLeftMotor.follow(m_frontLeftMotor);
-        m_backRightMotor.follow(m_frontRightMotor);
+    // Setup inversion
+    m_frontRightMotor.setInverted(true);
 
-        // Motors should brake when no active inputs are given
-        m_frontLeftMotor.setIdleMode(IdleMode.kBrake);
-        m_backLeftMotor.setIdleMode(IdleMode.kBrake);
-        m_frontRightMotor.setIdleMode(IdleMode.kBrake);
-        m_backRightMotor.setIdleMode(IdleMode.kBrake);
+    // Setup primary and follower controllers
+    m_backLeftMotor.follow(m_frontLeftMotor);
+    m_backRightMotor.follow(m_frontRightMotor);
 
-        // Burn! (i.e. flash the configs to the motors)
-        m_frontLeftMotor.burnFlash();
-        m_backLeftMotor.burnFlash();
-        m_frontRightMotor.burnFlash();
-        m_backRightMotor.burnFlash();
-    }
+    // Motors should brake when no active inputs are given
+    m_frontLeftMotor.setIdleMode(IdleMode.kBrake);
+    m_backLeftMotor.setIdleMode(IdleMode.kBrake);
+    m_frontRightMotor.setIdleMode(IdleMode.kBrake);
+    m_backRightMotor.setIdleMode(IdleMode.kBrake);
 
-    /**
-     * Run the drive train in arcade-drive mode.
-     *
-     * @param forward The forward velocity/movement
-     * @param rotation The rotation to use
-     */
-    public Command runArcadeDrive(DoubleSupplier forward, DoubleSupplier rotation) {
-        // Inline construction of command goes here.
-        // subsystem::run implicitly requires `this` subsystem.
-        return run(
-                () -> {
-                    m_drive.arcadeDrive(forward.getAsDouble() * Constants.MAX_SPEED, rotation.getAsDouble() * Constants.MAX_SPEED);
-                });
-    }
+    // Burn! (i.e. flash the configs to the motors)
+    m_frontLeftMotor.burnFlash();
+    m_backLeftMotor.burnFlash();
+    m_frontRightMotor.burnFlash();
+    m_backRightMotor.burnFlash();
+  }
 
-    @Override
-    public void periodic() {
-        SmartDashboard.putBoolean("at full speed", atFullSpeed.getAsBoolean());
-        SmartDashboard.putNumber("speed", getSpeed());
-    }
+  /**
+   * Run the drive train in arcade-drive mode.
+   *
+   * @param forward The forward velocity/movement
+   * @param rotation The rotation to use
+   */
+  public Command runArcadeDrive(DoubleSupplier forward, DoubleSupplier rotation) {
+    // Inline construction of command goes here.
+    // subsystem::run implicitly requires `this` subsystem.
+    return run(
+        () -> {
+          m_drive.arcadeDrive(
+              forward.getAsDouble() * Constants.MAX_SPEED,
+              rotation.getAsDouble() * Constants.MAX_SPEED);
+        });
+  }
 
-    public double getSpeed() {
-        return m_frontLeftMotor.get();
-    }
+  @Override
+  public void periodic() {
+    SmartDashboard.putBoolean("at full speed", atFullSpeed.getAsBoolean());
+    SmartDashboard.putNumber("speed", getSpeed());
+  }
+
+  public double getSpeed() {
+    return m_frontLeftMotor.get();
+  }
 }
